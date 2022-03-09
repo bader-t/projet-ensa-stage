9bf78153f6994e1c8e51a8e2663b470664c97215
asmae01 <asmaeoubraim331999@gmil.com>
2022-03-10 00:21:55 +0100

    add page 17

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
-import { Page2 } from "./pages";
-import { Page4 } from "./pages";
-import { Page16 } from "./pages";
+import { Page1, Page17, Page4,Page16 } from "./pages";
+//import {Page17} from "./pages/entreprise/page17";
+
 
 
 
 const App = () => (
 
-
-    <Page4></Page4>
+//<Page13>page 17</Page13>    
+<Page17></Page17>  
+//<Page4></Page4>
 );
 
 
